45e3c2cbbc675286fcd4410fb92fd834a5622e85
marcusungarette <marcusungarette@gmail.com>
2021-04-10 10:48:42 -0300

    Create Category entitie and route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import { Category } from "../model/category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    createdAt: new Date(),
+  });
+
+  categories.push(category);
+
+  return res.status(201).json({ category });
+});
+
+export { categoriesRoutes };
