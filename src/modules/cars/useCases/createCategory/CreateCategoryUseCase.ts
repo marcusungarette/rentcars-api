9b4a2b68bfed008b32ee75710c0efc7536775857
marcusungarette <marcusungarette@gmail.com>
2021-04-15 18:18:04 -0300

    Added singleton patter with tsrynge to createCategory

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,20 +1,25 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
-
+@injectable()
 class CreateCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
   async execute({ name, description }: IRequest): Promise<void> {
-    // const categoryAlreadyExists = await this.categoriesRepository.findByName(
-    //   name
-    // );
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
-    // if (categoryAlreadyExists) {
-    //   throw new Error("Category already exists");
-    // }
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists");
+    }
 
     this.categoriesRepository.create({ name, description });
   }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import createCategoryController from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import getAllCategoriesController from "../modules/cars/useCases/getAllCategories";
 import importCategoryController from "../modules/cars/useCases/importCategory";
 
@@ -11,9 +11,8 @@ const upload = multer({
   dest: "./tmp",
 });
 
-categoriesRoutes.post("/", (req, res) => {
-  return createCategoryController().handle(req, res);
-});
+const createCategoryController = new CreateCategoryController();
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
   return importCategoryController().handle(req, res);
