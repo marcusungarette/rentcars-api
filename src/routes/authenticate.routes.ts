8f69a92024c62fdcb2044ab6ed9fd1df44c9e04e
marcusungarette <marcusungarette@gmail.com>
2021-04-27 16:25:59 -0300

    AuthenticateUser: Create sessions route; check if user and password match to generate token

---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { AuthenticateUseController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUseController = new AuthenticateUseController();
+
+authenticateRoutes.post("/sessions", authenticateUseController.handle);
+
+export { authenticateRoutes };
